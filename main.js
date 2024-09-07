f17ed4d9ad7ea91e98e3c8e742ef225d9cb742fd
Hans Root <hroot@DESKTOP-ONUCPRC>
2024-09-08 02:33:13 +0300

    transform geojson to normal js object

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import Globe from "globe.gl";
+import { data } from "./ne_110m_admin_0_countries";
 
 // import * as d3 from "d3";
 // const colorScale = d3.scaleSequentialSqrt(d3.interpolateYlOrRd);
@@ -11,7 +12,7 @@ const markerSvg = `<svg viewBox="-4 0 36 36">
   </svg>`;
 
 const cords = [
-  { lat: 40.71427, long: -74.00597, name: "New York" },
+	{ lat: 40.71427, long: -74.00597, name: "New York" },
 	{ lat: 38.89511, long: -77.03637, name: "Washington D.C." },
 	{ lat: 34.88902, long: 35.88659, name: "Tartous" },
 	{ lat: 25.0657, long: 55.17128, name: "Dubai" },
@@ -25,58 +26,54 @@ const gData = cords.map((cord) => ({
 	color: ["red", "white", "blue", "green"][Math.round(Math.random() * 3)],
 }));
 
-fetch("./ne_110m_admin_0_countries.geojson")
-	.then((res) => res.json())
-	.then((countries) => {
-		const world = Globe()
-			.lights([])
-			.globeImageUrl("//unpkg.com/three-globe/example/img/earth-night.jpg")
-			.atmosphereColor("#00000000")
-			.showAtmosphere(false)
+const world = Globe()
+	.lights([])
+	.globeImageUrl("//unpkg.com/three-globe/example/img/earth-night.jpg")
+	.atmosphereColor("#00000000")
+	.showAtmosphere(false)
 
-			.polygonsData(countries.features)
-			.lineHoverPrecision(0)
+	.polygonsData(data.features)
+	.lineHoverPrecision(0)
 
-			// .polygonCapColor("#0f0")
+	// .polygonCapColor("#0f0")
 
-			.polygonCapColor(() => "#0f0")
+	.polygonCapColor(() => "#0f0")
 
-			.polygonAltitude(0.009)
+	.polygonAltitude(0.009)
 
-			// .polygonCapColor((feat) => colorScale(getVal(feat)))
-			.polygonSideColor(() => "#00000000")
-			.backgroundColor("#000")
-			// .polygonLabel(
-			// 	({ properties: d }) => `
-			//     <b>${d.ADMIN} (${d.ISO_A2}):</b> <br />
-			//     GDP: <i>${d.GDP_MD_EST}</i> M$<br/>
-			//     Population: <i>${d.POP_EST}</i>
-			//   `
-			// )
-			.onPolygonHover((hoverD) =>
-				world
-					.polygonAltitude((d) => (d === hoverD ? 0.04 : 0.009))
-					.polygonCapColor((d) =>
-						// d === hoverD ? "steelblue" : colorScale(getVal(d))
-						d === hoverD ? "steelblue" : "#0f0"
-					)
+	// .polygonCapColor((feat) => colorScale(getVal(feat)))
+	.polygonSideColor(() => "#00000000")
+	.backgroundColor("#000")
+	// .polygonLabel(
+	// 	({ properties: d }) => `
+	//     <b>${d.ADMIN} (${d.ISO_A2}):</b> <br />
+	//     GDP: <i>${d.GDP_MD_EST}</i> M$<br/>
+	//     Population: <i>${d.POP_EST}</i>
+	//   `
+	// )
+	.onPolygonHover((hoverD) =>
+		world
+			.polygonAltitude((d) => (d === hoverD ? 0.04 : 0.009))
+			.polygonCapColor((d) =>
+				// d === hoverD ? "steelblue" : colorScale(getVal(d))
+				d === hoverD ? "steelblue" : "#0f0"
 			)
-			.polygonStrokeColor(() => "#111")
-			.htmlElementsData(gData)
-			.htmlElement((d) => {
-				const el = document.createElement("div");
-				el.innerHTML = markerSvg;
-				el.style.color = d.color;
-				el.style.width = `${d.size}px`;
+	)
+	.polygonStrokeColor(() => "#111")
+	.htmlElementsData(gData)
+	.htmlElement((d) => {
+		const el = document.createElement("div");
+		el.innerHTML = markerSvg;
+		el.style.color = d.color;
+		el.style.width = `${d.size}px`;
 
-				el.style["pointer-events"] = "auto";
-				el.style.cursor = "pointer";
-				el.onclick = () => console.info(d);
-				return el;
-			})(document.getElementById("globeViz"));
+		el.style["pointer-events"] = "auto";
+		el.style.cursor = "pointer";
+		el.onclick = () => console.info(d);
+		return el;
+	})(document.getElementById("globeViz"));
 
-		const globeMaterial = world.globeMaterial();
-		// globeMaterial.alphaHash = true
-		globeMaterial.transparent = true;
-		globeMaterial.opacity = 0.9;
-	});
+const globeMaterial = world.globeMaterial();
+// globeMaterial.alphaHash = true
+globeMaterial.transparent = true;
+globeMaterial.opacity = 0.9;
