89638e4f6dba7bd85d7e018c7d59d470dfff786d
Hans Root <hroot@DESKTOP-ONUCPRC>
2024-09-08 17:04:21 +0300

    rotate animation and pov click to country coords

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import Globe from "globe.gl";
 import { data } from "./ne_110m_admin_0_countries";
 
+let selected = { lat: 0, lng: 0, name: "" };
+
 // import * as d3 from "d3";
 // const colorScale = d3.scaleSequentialSqrt(d3.interpolateYlOrRd);
 // const getVal = (feat) =>
@@ -19,31 +21,44 @@ const cords = [
 	{ lat: 25.33737, long: 55.41206, name: "Sharjah" },
 ];
 
+const button = (lat, lng, name) =>
+	`<button class="go-btn" data-lat="${lat}" data-lng="${lng}" data-name="${name}">Go To ${name}</button>`;
+
+const btnContainer = document.getElementById("fill-btns");
+btnContainer.innerHTML = cords
+	.map((cord) => {
+		return button(cord.lat, cord.long, cord.name);
+	})
+	.join("");
+
 const gData = cords.map((cord) => ({
 	lat: cord.lat,
 	lng: cord.long,
 	size: 30,
-	color: ["red", "white", "blue", "green"][Math.round(Math.random() * 3)],
+	color: "#317bfe",
+	// color: ["red", "white", "blue", "green"][Math.round(Math.random() * 3)],
 }));
+
+const btn = document.querySelectorAll(".go-btn");
 
 const world = Globe()
 	.lights([])
 	.globeImageUrl("//unpkg.com/three-globe/example/img/earth-night.jpg")
-	.atmosphereColor("#00000000")
+	// .atmosphereColor("#00000000")
 	.showAtmosphere(false)
 
 	.polygonsData(data.features)
 	.lineHoverPrecision(0)
 
-	// .polygonCapColor("#0f0")
+	// .polygonCapColor("#10b650")
 
-	.polygonCapColor(() => "#0f0")
+	.polygonCapColor(() => "#10b650")
 
 	.polygonAltitude(0.009)
 
 	// .polygonCapColor((feat) => colorScale(getVal(feat)))
 	.polygonSideColor(() => "#00000000")
-	.backgroundColor("#000")
+	.backgroundColor("#00000000")
 	// .polygonLabel(
 	// 	({ properties: d }) => `
 	//     <b>${d.ADMIN} (${d.ISO_A2}):</b> <br />
@@ -53,10 +68,12 @@ const world = Globe()
 	// )
 	.onPolygonHover((hoverD) =>
 		world
-			.polygonAltitude((d) => (d === hoverD ? 0.04 : 0.009))
+			.polygonAltitude((d) => {
+				return d === hoverD ? 0.04 : 0.009;
+			})
 			.polygonCapColor((d) =>
 				// d === hoverD ? "steelblue" : colorScale(getVal(d))
-				d === hoverD ? "steelblue" : "#0f0"
+				d === hoverD ? "#fe3c3b" : "#10b650"
 			)
 	)
 	.polygonStrokeColor(() => "#111")
@@ -69,11 +86,57 @@ const world = Globe()
 
 		el.style["pointer-events"] = "auto";
 		el.style.cursor = "pointer";
-		el.onclick = () => console.info(d);
+		el.onclick = () => {
+			world.pointOfView(
+				{
+					lat: d.lat,
+					lng: d.lng,
+					altitude: 0.9,
+				},
+				1000
+			);
+		};
 		return el;
 	})(document.getElementById("globeViz"));
 
+window.addEventListener("resize", (event) => {
+	world.width((event.target.innerWidth / 3) * 2);
+	world.height(event.target.innerHeight);
+});
+
 const globeMaterial = world.globeMaterial();
-// globeMaterial.alphaHash = true
 globeMaterial.transparent = true;
-globeMaterial.opacity = 0.9;
+globeMaterial.opacity = 0.6;
+
+world.controls().autoRotate = true;
+world.controls().autoRotateSpeed = 1.8;
+
+btn.forEach((element) => {
+	element.addEventListener("click", (e) => {
+		btn.forEach((item) => (item.disabled = "true"));
+		world.resumeAnimation();
+		const temp = {
+			lat: +e.target.getAttribute("data-lat"),
+			lng: +e.target.getAttribute("data-lng"),
+			name: +e.target.getAttribute("data-name"),
+		};
+		if (selected.lat === temp.lat && selected.lng === temp.lng) {
+			selected = {};
+		} else {
+			selected.lat = temp.lat;
+			selected.lng = temp.lng;
+			world.pointOfView(
+				{
+					lat: temp.lat,
+					lng: temp.lng,
+					altitude: 2,
+				},
+				1000
+			);
+			setTimeout(() => {
+				btn.forEach((item) => (item.disabled = null));
+				world.pauseAnimation();
+			}, 1000);
+		}
+	});
+});
