e7a9b992ecc08807d17d84f1c82652f6825f5cca
Hans Root <sudo.hroot@gmail.com>
2024-10-31 18:14:40 +0300

    update fix button clicks and selected elements

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ import * as turf from "@turf/turf";
 // <script src="//unpkg.com/globe.gl"></script>
 // <script src="https://cdn.jsdelivr.net/npm/@turf/turf@7/turf.min.js"></script>
 
-// global state/vars
+//#region Elements & Constants
 let selected = { lat: 0, lng: 0, name: "" };
 const world = Globe();
 
@@ -16,19 +16,29 @@ const RED_COLOR = "#b11116";
 const FOCUSED_HEIGHT = 0.04;
 const UN_FOCUS_HEIGHT = 0.009;
 
-const PHONE_ICON =
-	'<svg xmlns="http://www.w3.org/2000/svg" width="15px" height="15px" viewBox="0 0 56 56"><path fill="currentColor" d="M39.156 50.934c4.078 0 6.774-1.102 9.164-3.774c.188-.187.352-.398.54-.586c1.406-1.57 2.062-3.117 2.062-4.593c0-1.688-.984-3.258-3.07-4.712l-6.82-4.734c-2.11-1.453-4.571-1.617-6.54.328l-1.804 1.805c-.54.539-1.008.563-1.547.234c-1.242-.797-3.797-3.023-5.532-4.757c-1.828-1.805-3.609-3.82-4.523-5.297c-.328-.54-.281-.985.258-1.524l1.781-1.805c1.969-1.968 1.805-4.453.352-6.538l-4.758-6.82c-1.43-2.087-3-3.048-4.688-3.071c-1.476-.024-3.023.656-4.593 2.062c-.211.188-.399.352-.61.516c-2.648 2.39-3.75 5.086-3.75 9.14c0 6.704 4.125 14.86 11.696 22.43c7.523 7.524 15.703 11.696 22.382 11.696m.024-3.61c-5.977.117-13.64-4.476-19.711-10.523c-6.117-6.094-10.922-14.016-10.805-19.992c.047-2.579.938-4.805 2.79-6.399c.14-.14.28-.258.444-.375c.68-.61 1.454-.937 2.11-.937c.703 0 1.312.257 1.758.96l4.547 6.82c.492.727.539 1.548-.188 2.274l-2.062 2.063c-1.641 1.617-1.5 3.586-.328 5.156c1.335 1.805 3.656 4.43 5.437 6.211c1.805 1.805 4.64 4.336 6.445 5.695c1.57 1.172 3.563 1.29 5.18-.328l2.062-2.062c.727-.727 1.524-.68 2.25-.211l6.82 4.547c.704.468.985 1.054.985 1.758c0 .68-.328 1.43-.96 2.132a5.82 5.82 0 0 1-.352.446c-1.617 1.828-3.844 2.718-6.422 2.765"/></svg>';
-const EMAIL_ICON =
-	'<svg xmlns="http://www.w3.org/2000/svg" width="15px" height="15px" viewBox="0 0 16 16"><path fill="currentColor" fill-rule="evenodd" d="M14.95 3.684L8.637 8.912a1 1 0 0 1-1.276 0l-6.31-5.228A.999.999 0 0 0 1 4v8a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4a.999.999 0 0 0-.05-.316M2 2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2m-.21 1l5.576 4.603a1 1 0 0 0 1.27.003L14.268 3z"/></svg>';
-const LOCATION_ICON =
-	'<svg xmlns="http://www.w3.org/2000/svg" width="15px" height="15px" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><path d="M12.56 20.82a.964.964 0 0 1-1.12 0C6.611 17.378 1.486 10.298 6.667 5.182A7.592 7.592 0 0 1 12 3c2 0 3.919.785 5.333 2.181c5.181 5.116.056 12.196-4.773 15.64Z"/><path d="M12 12a2 2 0 1 0 0-4a2 2 0 0 0 0 4Z"/></g></svg>';
-
 // Elements
 // Me: Mama I want React.js
 // Mama: We Have React.js At Home
 // React.js at home:
-const MarkerSvg = ({ lat, lng, name, flag, type, phone, email, location }) => `
-<button class="infoButton" data-name="${name}">
+const PHONE_ICON =
+	'<svg xmlns="http://www.w3.org/2000/svg" width="15px" height="15px" viewBox="0 0 56 56"><path fill="currentColor" d="M39.156 50.934c4.078 0 6.774-1.102 9.164-3.774c.188-.187.352-.398.54-.586c1.406-1.57 2.062-3.117 2.062-4.593c0-1.688-.984-3.258-3.07-4.712l-6.82-4.734c-2.11-1.453-4.571-1.617-6.54.328l-1.804 1.805c-.54.539-1.008.563-1.547.234c-1.242-.797-3.797-3.023-5.532-4.757c-1.828-1.805-3.609-3.82-4.523-5.297c-.328-.54-.281-.985.258-1.524l1.781-1.805c1.969-1.968 1.805-4.453.352-6.538l-4.758-6.82c-1.43-2.087-3-3.048-4.688-3.071c-1.476-.024-3.023.656-4.593 2.062c-.211.188-.399.352-.61.516c-2.648 2.39-3.75 5.086-3.75 9.14c0 6.704 4.125 14.86 11.696 22.43c7.523 7.524 15.703 11.696 22.382 11.696m.024-3.61c-5.977.117-13.64-4.476-19.711-10.523c-6.117-6.094-10.922-14.016-10.805-19.992c.047-2.579.938-4.805 2.79-6.399c.14-.14.28-.258.444-.375c.68-.61 1.454-.937 2.11-.937c.703 0 1.312.257 1.758.96l4.547 6.82c.492.727.539 1.548-.188 2.274l-2.062 2.063c-1.641 1.617-1.5 3.586-.328 5.156c1.335 1.805 3.656 4.43 5.437 6.211c1.805 1.805 4.64 4.336 6.445 5.695c1.57 1.172 3.563 1.29 5.18-.328l2.062-2.062c.727-.727 1.524-.68 2.25-.211l6.82 4.547c.704.468.985 1.054.985 1.758c0 .68-.328 1.43-.96 2.132a5.82 5.82 0 0 1-.352.446c-1.617 1.828-3.844 2.718-6.422 2.765"/></svg>';
+const EMAIL_ICON =
+	'<svg xmlns="http://www.w3.org/2000/svg" width="15px" height="15px" viewBox="0 0 24 24"><path fill="currentColor" d="M5 5h13a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V8a3 3 0 0 1 3-3m0 1c-.5 0-.94.17-1.28.47l7.78 5.03l7.78-5.03C18.94 6.17 18.5 6 18 6H5m6.5 6.71L3.13 7.28C3.05 7.5 3 7.75 3 8v9a2 2 0 0 0 2 2h13a2 2 0 0 0 2-2V8c0-.25-.05-.5-.13-.72l-8.37 5.43Z"/></svg>';
+const LOCATION_ICON =
+	'<svg xmlns="http://www.w3.org/2000/svg" width="15px" height="15px" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"><path d="M12.56 20.82a.964.964 0 0 1-1.12 0C6.611 17.378 1.486 10.298 6.667 5.182A7.592 7.592 0 0 1 12 3c2 0 3.919.785 5.333 2.181c5.181 5.116.056 12.196-4.773 15.64Z"/><path d="M12 12a2 2 0 1 0 0-4a2 2 0 0 0 0 4Z"/></g></svg>';
+
+const MarkerSvg = ({
+	lat,
+	lng,
+	name,
+	flag,
+	type,
+	phone,
+	email,
+	location,
+	sub,
+}) => `
+<button class="infoButton" data-name="${sub}">
 	<svg id="animate" viewBox="-4 0 36 36">
 		<path data-lat="${lat}" data-lng="${lng}" fill="${COLOR_CURSOR}" d="M14,0 C21.732,0 28,5.641 28,12.6 C28,23.963 14,36 14,36 C14,36 0,24.064 0,12.6 C0,5.641 6.268,0 14,0 Z"></path>
 	</svg>
@@ -47,7 +57,7 @@ const MarkerSvg = ({ lat, lng, name, flag, type, phone, email, location }) => `
 					<span>${phone}</span>
 				</span>
 				<span style="display: flex; gap: 5px; align-items: center">
-					<span style="stroke: ${RED_COLOR}; fill: ${RED_COLOR};">${EMAIL_ICON}</span>
+					<span style="stroke: ${RED_COLOR}">${EMAIL_ICON}</span>
 					<span>${email}</span>
 				</span>
 				<span style="display: flex; gap: 5px; align-items: center">
@@ -62,6 +72,7 @@ const MarkerSvg = ({ lat, lng, name, flag, type, phone, email, location }) => `
 
 const Button = (lat, lng, name) =>
 	`<button class="go-btn" data-lat="${lat}" data-lng="${lng}" data-name="${name}">Go To ${name}</button>`;
+//#endregion Elements
 
 // Coords for labels and navigation
 // todo set the coords here :)
@@ -78,7 +89,7 @@ const cords = [
 			type: "Headquarters",
 			phone: "+1 123 123 123",
 			email: "us.a@scagroup.net",
-			location: "New York, United States of America 🦅🛢️💣",
+			location: "New York, United States of America",
 		},
 	},
 	{
@@ -93,7 +104,7 @@ const cords = [
 			type: "Headquarters",
 			phone: "+963 123 123 123",
 			email: "sy.a@scagroup.net",
-			location: "Tartous, Syria 🧉💀🏖️",
+			location: "Tartous, Syria",
 		},
 	},
 	{
@@ -108,7 +119,7 @@ const cords = [
 			type: "Headquarters",
 			phone: "+963 123 123 123",
 			email: "ae.a@scagroup.net",
-			location: "Dubai, UAE 🏜️🛢️💰",
+			location: "Dubai, UAE",
 		},
 	},
 	{
@@ -123,7 +134,7 @@ const cords = [
 			type: "Headquarters",
 			phone: "+963 123 123 123",
 			email: "it.a@scagroup.net",
-			location: "Milan, Italy 🍕🍷🤌",
+			location: "Milan, Italy",
 		},
 	},
 	{
@@ -138,7 +149,7 @@ const cords = [
 			type: "Headquarters",
 			phone: "+963 123 123 123",
 			email: "de.a@scagroup.net",
-			location: "Frankfurt, Germany 🍺🏎️",
+			location: "Frankfurt, Germany",
 		},
 	},
 	{
@@ -153,7 +164,7 @@ const cords = [
 			type: "Headquarters",
 			phone: "+963 123 123 123",
 			email: "de.a@scagroup.net",
-			location: "London, United Kingdom 🍵🎩🔪",
+			location: "London, United Kingdom",
 		},
 	},
 	{
@@ -168,7 +179,22 @@ const cords = [
 			type: "Headquarters",
 			phone: "+963 123 123 123",
 			email: "ru.a@scagroup.net",
-			location: "Moscow, Russia 🍹🔫",
+			location: "Moscow, Russia",
+		},
+	},
+	{
+		lat: -33.8688,
+		long: 151.2093,
+		name: "Sidney",
+		info: {
+			country: {
+				name: "Australia",
+				flag: "https://cdn.jsdelivr.net/npm/country-flag-emoji-json@2.0.0/dist/images/AU.svg",
+			},
+			type: "Headquarters",
+			phone: "+963 123 123 123",
+			email: "au.a@scagroup.net",
+			location: "Sidney, Australia",
 		},
 	},
 ];
@@ -185,6 +211,7 @@ const gData = cords.map((cord) => ({
 	phone: cord.info.phone,
 	location: cord.info.location,
 	type: cord.info.type,
+	sub: cord.name,
 }));
 
 const handleUpdateGlobe = (d = { lat: 0, lng: 0 }) => {
@@ -222,6 +249,87 @@ const resizer = () => {
 	world.height(window.innerHeight);
 };
 
+// pointer on map click helper
+const handleButtonClick = (el, d) => {
+	const btn = document.querySelectorAll(".go-btn");
+	world.resumeAnimation();
+	btn.forEach((item) => (item.disabled = true));
+
+	const clickedPointer = document.querySelector(
+		`button[data-name='${d.sub}'].infoButton`
+	);
+
+	const classes = clickedPointer.classList;
+	const isActive = clickedPointer.classList.contains("infoButton_isActive");
+
+	document.querySelectorAll(".infoButton").forEach((btn) => {
+		btn.classList.remove("infoButton_isActive");
+	});
+
+	if (isActive) {
+		classes.remove("infoButton_isActive");
+	} else {
+		classes.add("infoButton_isActive");
+	}
+
+	if (selected.lat == d.lat && selected.lng == d.lng) {
+		world.pointOfView(
+			{
+				lat: d.lat,
+				lng: d.lng,
+				altitude: 2,
+			},
+			1000
+		);
+		handleUpdateGlobe({ lat: 0, lng: 0 });
+		setTimeout(() => {
+			btn.forEach((item) => (item.disabled = null));
+		}, 1000);
+		selected = {};
+	} else {
+		world.resumeAnimation();
+		world.pointOfView(
+			{
+				lat: d.lat,
+				lng: d.lng,
+				altitude: 2,
+			},
+			1000
+		);
+		selected = {
+			lat: d.lat,
+			lng: d.lng,
+		};
+		handleUpdateGlobe(selected);
+		setTimeout(() => {
+			btn.forEach((item) => (item.disabled = null));
+			world.pauseAnimation();
+		}, 1000);
+	}
+};
+
+// btn click helper
+const handleToggleMenus = (nodeData) => {
+	setTimeout(() => {
+		const selectedElement = document.querySelector(
+			`button[data-name='${nodeData.name}'].infoButton`
+		);
+
+		const classes = selectedElement.classList;
+		const isActive = selectedElement.classList.contains("infoButton_isActive");
+
+		document.querySelectorAll(".infoButton").forEach((btn) => {
+			btn.classList.remove("infoButton_isActive");
+		});
+
+		if (isActive) {
+			classes.remove("infoButton_isActive");
+		} else {
+			classes.add("infoButton_isActive");
+		}
+	}, 1000);
+};
+
 const renderButtons = () => {
 	const btnContainer = document.getElementById("fill-btns");
 	btnContainer.innerHTML = cords
@@ -244,67 +352,12 @@ const renderer = () => {
 		.polygonStrokeColor(() => "#111")
 		.htmlElementsData(gData)
 		.htmlElement((d) => {
-			const btn = document.querySelectorAll(".go-btn");
 			const el = document.createElement("div");
-			console.log(d);
 			el.innerHTML = MarkerSvg({ ...d });
 
 			el.style["pointer-events"] = "auto";
 			el.style.cursor = "pointer";
-			el.onclick = () => {
-				world.resumeAnimation();
-				btn.forEach((item) => (item.disabled = true));
-
-				const classes = el.classList;
-				const isActive = el.classList.contains("infoButton_isActive");
-
-				document.querySelectorAll("#animate").forEach((btn) => {
-					btn.parentElement.parentElement.classList.remove(
-						"infoButton_isActive"
-					);
-				});
-
-				if (isActive) {
-					classes.remove("infoButton_isActive");
-				} else {
-					classes.add("infoButton_isActive");
-				}
-
-				if (selected.lat == d.lat && selected.lng == d.lng) {
-					world.pointOfView(
-						{
-							lat: d.lat,
-							lng: d.lng,
-							altitude: 2,
-						},
-						1000
-					);
-					handleUpdateGlobe({ lat: 0, lng: 0 });
-					setTimeout(() => {
-						btn.forEach((item) => (item.disabled = null));
-					}, 1000);
-					selected = {};
-				} else {
-					world.resumeAnimation();
-					world.pointOfView(
-						{
-							lat: d.lat,
-							lng: d.lng,
-							altitude: 2,
-						},
-						1000
-					);
-					selected = {
-						lat: d.lat,
-						lng: d.lng,
-					};
-					handleUpdateGlobe(selected);
-					setTimeout(() => {
-						btn.forEach((item) => (item.disabled = null));
-						world.pauseAnimation();
-					}, 1000);
-				}
-			};
+			el.onclick = () => handleButtonClick(el, d);
 
 			return el;
 		})(document.getElementById("globeViz"));
@@ -329,37 +382,16 @@ const mapButtons = () => {
 
 	btn.forEach((element) => {
 		element.addEventListener("click", (e) => {
-			console.log("clicked");
 			btn.forEach((item) => (item.disabled = "true"));
 			world.resumeAnimation();
+
 			const temp = {
 				lat: +e.target.getAttribute("data-lat"),
 				lng: +e.target.getAttribute("data-lng"),
-				name: +e.target.getAttribute("data-name"),
+				name: e.target.getAttribute("data-name"),
 			};
 
-			// const selectedElement = document.querySelector(
-			// 	`path[data-lat='${temp.lat}'], path[data-lng='${temp.lng}']`
-			// );
-
-			// const isActive =
-			// 	selectedElement.parentElement.parentElement.classList.contains(
-			// 		"infoButton_isActive"
-			// 	);
-
-			// document.querySelectorAll("#animate").forEach((btn) => {
-			// 	btn.parentElement.parentElement.classList.remove("infoButton_isActive");
-			// });
-
-			// if (isActive) {
-			// 	selectedElement.parentElement.parentElement.classList.remove(
-			// 		"infoButton_isActive"
-			// 	);
-			// } else {
-			// 	selectedElement.parentElement.parentElement.classList.add(
-			// 		"infoButton_isActive"
-			// 	);
-			// }
+			handleToggleMenus(temp);
 
 			if (selected.lat === temp.lat && selected.lng === temp.lng) {
 				world.pointOfView(
